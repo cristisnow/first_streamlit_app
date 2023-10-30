29fcca3c9db7b56ba49932b073e8f2c9e0e8e85f
cristisnow <149249645+cristisnow@users.noreply.github.com>
2023-10-29 21:30:29 -0700

    Update streamlit_app.py
    error

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,15 +27,17 @@ streamlit.dataframe(fruits_to_show)
 streamlit.stop()
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -45,7 +47,7 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-add_my_fruit = streamlit.multiselect("Waht fruuit would you like to add", list(my_fruit_list.index))
+add_my_fruit = streamlit.multiselect("Waht fruit would you like to add", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[add_my_fruit]
 
 streamlit.write('Thanks for adding ', add_my_fruit)
